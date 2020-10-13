f942a56c0828a39b72c21a6be1f15da20740bd1c
casallas123 <andresf-casallasa@unilibre.edu.co>
2020-10-13 15:46:59 -0500

    mapita Clase24

--- app/src/main/java/com/example/proyectoa20202/MapClientAct.java
+++ app/src/main/java/com/example/proyectoa20202/MapClientAct.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.security.AuthProvider;
+
 public class MapClientAct extends AppCompatActivity {
+
+    AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
+
+
     }
 }
--- app/src/main/java/com/example/proyectoa20202/MapDriverAct.java
+++ app/src/main/java/com/example/proyectoa20202/MapDriverAct.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MapDriverAct extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapDriverAct extends AppCompatActivity  implements OnMapReadyCallback {
+
+    private  GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
 }
